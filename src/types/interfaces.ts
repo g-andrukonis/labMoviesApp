d407ae467f08382061fdbf6c6b4f3fd2a444f722
[Gediminas] <[g.andrukonis@gmail.com]>
2025-08-31 17:16:57 +0100

    movie details page component

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -17,3 +17,10 @@ export interface BaseMovieProps {
     favourite?: boolean;
     movies: BaseMovieProps[];
   }
+
+  export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+  }
