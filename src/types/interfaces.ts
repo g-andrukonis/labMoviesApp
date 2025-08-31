8b535a27bb44b50d1a8bbc35b8c0077a8265428f
[Gediminas] <[g.andrukonis@gmail.com]>
2025-08-31 18:49:33 +0100

    show movie review excerpts, refactor details page

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -51,3 +51,9 @@ export interface BaseMovieListProps {
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
   title: string;
 }
+
+export interface Review{
+  id: string;
+  content: string
+  author: string
+}
